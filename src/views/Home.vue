b4e715999f61e27183568dc949ebb62422a04264
aixuexi <2076894460@qq.com>
2021-07-12 00:36:42 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <top-view />
+  </div>
+</template>
+
+<script>
+
+import TopView from "../components/TowView/index";
+export default {
+  name: 'Home',
+  components: {
+    TopView
+  }
+}
+</script>
