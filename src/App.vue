7b7696f1a4b60c2fe07f3f2c92cf281ac6fe18e9
zpz <2076894460@qq.com>
2021-07-24 15:44:54 +0800

    'end'

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,30 @@
 </template>
 <script>
 export default {
+  created () {
+    let arr = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '0' }, { id: '4' }, { id: '5' }, { id: '1' }, { id: '2' }]
+    let newArr = arr.map(v => v.id)
+    let result = new Array();
+    newArr.forEach((item, index) => {
+      if (newArr.indexOf(item) !== newArr.lastIndexOf(item) && !result.find(v => v.id == arr[index].id)) {
+        result.push(arr[index])
+      }
+    })
+    console.warn(result);
 
-}
+
+
+
+    //   console.log(arr);
+    //   let newArr = new Array();
+    //   arr.forEach(item => {
+    //     if (!newArr.find(i => i.name == item.name)) {
+    //       newArr.push(item)
+    //     }
+    //   })
+    //   console.log(newArr);//[{ name: '1' },{ name: '2' }]
+  }}
+
 </script>
 
 
--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,19 +1,111 @@
 <template>
-  <ve-line :data="chartData"></ve-line>
+  <div class="sales-view">
+    <el-card>
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu mode="horizontal"
+                   :default-active="activeIndex"
+                   @select="onMenuSelect"
+                   class="sales-menu">
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect"
+                            size="small">
+              <el-radio-button label="今日" />
+              <el-radio-button label="本周" />
+              <el-radio-button label="本月" />
+              <el-radio-button label="今年" />
+            </el-radio-group>
+            <el-date-picker type="daterange"
+                            v-model="date"
+                            range-separator="至"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期"
+                            size="small"
+                            unlink-panels
+                            class="sales-date"
+                            :picker-options="pickerOptions">
+
+            </el-date-picker>
+          </div>
+        </div>
+      </template>
+      <template>11</template>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      chartData: {
-        columns: ['日期', '访问用户', '下单用户'],
-        rows: [
-          { '日期': '2018-05-22', '访问用户': 32371, '下单用户': 19810 },
-          { '日期': '2018-05-23', '访问用户': 12328, '下单用户': 4398 },
-          { '日期': '2018-05-24', '访问用户': 92381, '下单用户': 52910 }
-        ]
+      activeIndex: '1',
+      radioSelect: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const start = new Date();
+            const end = new Date();
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+            picker.$emit('pick', [start, end], true)
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const start = new Date();
+            const end = new Date();
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 30);
+            picker.$emit('pick', [start, end], false)
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const start = new Date();
+            const end = new Date();
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 90);
+            picker.$emit('pick', [start, end], false)
+          }
+        }]
       }
+    }
+  },
+  methods: {
+    onMenuSelect (index) {
+      this.activeIndex = index
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.sales-view{
+  margin-top: 20px;
+  .menu-wrapper{
+    position: relative;
+    display: flex;
+    .sales-menu{
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item{
+        height: 50px;
+        line-height: 50px;
+        margin:  0 20px
+      }
+    }
+    .menu-right{
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: end;
+      .sales-date{
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
